5273c953f54dd10474932d6e88340beb7afbbe94
jaeyoung <ckw0600@hansung.ac.kr>
2019-11-29 17:34:15 +0900

    Second commit

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 public class hello{
 	public static void main(String[] args){
-		System.out.println("Hello, world from GIT!");
+        System.out.println("Hello, world from GIT!");
+        System.out.println("Changes after First commit");		
 	}
 }
 
